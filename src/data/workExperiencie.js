171b56d2bca3a62f3d1374f6b2883f68d117455a
Sergio Gachuz <sergioegch@hotmail.com>
2024-09-04 23:29:44 -0600

    Maquetación WorkSection

---
+++ src/data/workExperiencie.js
@@ -0,0 +1,62 @@
+export const workExperiencie = [
+  {
+    id: 1,
+    position: 'Centers Developer',
+    company: 'NTT Data',
+    time: '2022 - Act',
+    activities: 'Colaboración con Telefónica México Ecommerce realizando las siguientes actividades: Integración y modificación de componentes en los diversos flujos. Maquetación de landing pages y correos. Migración de APIS. Construcción de importadores para procesamiento de ordenes. Capacitaciones de temas técnicos de Magento',
+    techStack: [
+      {
+        name: 'Magento'
+      },
+      {
+        name: 'Php'
+      },
+      {
+        name: 'Xml'
+      },
+      {
+        name: 'Git'
+      },
+      {
+        name: 'MySql'
+      },
+      {
+        name: 'Html'
+      },
+      {
+        name: 'Javascript'
+      },
+      {
+        name: 'Css'
+      },
+    ]
+  },
+  {
+    id: 2,
+    position: 'Desarrollador Web',
+    company: 'Ayuntamiento de Celaya',
+    time: '2020 - 2021',
+    activities: 'Participación en el desarrollo del Campus Virtual. Líder del equipo de sistemas dentro de la dependencia.Programación y soporte de video conferencias para las capacitaciones virtuales de los empleados.',
+    techStack: [
+      {
+        name: 'Php'
+      },
+      {
+        name: 'MySql'
+      },
+      {
+        name: 'Html'
+      },
+      {
+        name: 'Javascript'
+      },
+      {
+        name: 'Css'
+      },
+      {
+        name: 'Bootstrap'
+      },
+    ]
+  },
+]
---
+++ src/sections/WorkSection.jsx
@@ -0,0 +1,14 @@
+import { TimeLine, TitleSection } from '../components'
+
+export const WorkSection = () => {
+  return (
+    <section className="other-section">
+      <TitleSection title="EXPERIENCIA LABORAL"/>
+      <div className="section-content">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ex error tenetur cum illo! Eius omnis cum explicabo incidunt laudantium laborum, voluptatem quas doloribus, eveniet harum ipsum illo esse commodi.
+        A labore commodi, asperiores dolorem velit laboriosam libero doloribus unde perferendis laborum iste repellendus dolor, voluptatibus eveniet maiores deleniti aspernatur obcaecati quaerat. Veritatis similique cum, eum corporis rem mollitia rerum?</p>
+        <TimeLine />
+      </div>
+    </section>
+  )
+}
